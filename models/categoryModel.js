fe560f4258da07a6704ca1a5304cfdc07e80b317
paygood15 <167597519+paygood15@users.noreply.github.com>
2024-04-29 10:49:34 +0300

    Update categoryModel.js

--- models/categoryModel.js
+++ models/categoryModel.js
@@ -11,7 +11,7 @@ const categorySchema = new mongoose.Schema(
       minlength: [3, 'Too short category name'],
       maxlength: [32, 'Too long category name'],
     },
-        nameAR: {
+        nameAr: {
       type: String,
       trim: true,
       // index: true,
